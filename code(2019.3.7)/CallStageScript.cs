fbfa735a87587a2e7170902a37a1aef0c40b8864
arisun000 <48049422+Arisun000@users.noreply.github.com>
2019-03-07 22:55:46 +0900

    問題点
    Enemy1Script.cs
    BombScript.cs
    PlayerScript.cs(129行)

---
+++ code(2019.3.7)/CallStageScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CallStageScript : MonoBehaviour {
+	public Text title;
+	public Text Key;
+	private int i = 1;
+	void Update ()
+	{
+		if (i == 1 && Input.GetKeyDown("space")) {
+			title.text = "攻撃:左Ctrlキー\n\n移動:矢印キー\n\nジャンプ:Spaceキー";
+			Key.text ="---Press to Left Cotrol Key---";
+			i++;
+		}
+		if(i == 2 && Input.GetKeyDown("left ctrl")) {
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
---
+++ code(2019.3.7)/Enemy1Script.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy1Script : MonoBehaviour {
+	
+	private new Rigidbody2D rigidbody2D;
+	public float speed = -3f;
+	public GameObject explosion;
+	public bool Bombs = false;
+	public GameObject BombObject;
+
+	private const string MAIN_CAMERA_TAG_NAME = "MainCamera";
+	public bool _isRendered = false;
+
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+					Bombs = BombObject.GetComponent<BombScript> ().Bombjudge;
+		if(_isRendered) {
+			rigidbody2D.velocity = new Vector2 (transform.localScale.x * speed, rigidbody2D.velocity.y);
+		} else {
+			rigidbody2D.velocity = new Vector2 (0,0);
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "Bullet") {
+			FindObjectOfType<ScoreScript>().Addpoint(10);
+			Destroy (gameObject);
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
+	}
+	void OnTriggerStay2D (Collider2D col) 
+	{
+		if (col.gameObject.tag == "Bomb") {
+			if(Bombs == true && Input.GetKeyDown("left ctrl")) {
+			FindObjectOfType<ScoreScript>().Addpoint(10);
+			Destroy (gameObject);
+			Instantiate (explosion, transform.position, transform.rotation);				
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{	
+		if (col.gameObject.tag == "Block" || col.gameObject.tag == "Enemy") {
+			Vector2 temp = gameObject.transform.localScale;
+			temp.x *= -1;
+			gameObject.transform.localScale = temp;
+		}
+	}
+
+		//Rendererがカメラに映ってる間に呼ばれ続ける
+	void OnWillRenderObject() {
+    //メインカメラに映った時だけ_isRenderedをtrue
+		if(Camera.current.tag == MAIN_CAMERA_TAG_NAME) _isRendered = true;
+	}
+}
